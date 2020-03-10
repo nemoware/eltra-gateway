ac69962e9e741cd4bc6239864eb6ccf64189ee2a
thingumajig <artifex.i@gmail.com>
2020-03-10 16:05:40 +0300

    * remove unresolved routing

--- src/main/webapp/app/entities/entity.module.ts
+++ src/main/webapp/app/entities/entity.module.ts
@@ -4,10 +4,10 @@ import { RouterModule } from '@angular/router';
 @NgModule({
   imports: [
     RouterModule.forChild([
-      {
-        path: 'weather',
-        loadChildren: () => import('./weathersample/weather/weather.module').then(m => m.WeathersampleWeatherModule)
-      }
+      // {
+      //   path: 'weather',
+      //   loadChildren: () => import('./weathersample/weather/weather.module').then(m => m.WeathersampleWeatherModule)
+      // }
       /* jhipster-needle-add-entity-route - JHipster will add entity modules routes here */
     ])
   ]
