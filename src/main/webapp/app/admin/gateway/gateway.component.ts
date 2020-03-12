b1cab4ff7c2f6fcb87fb83b11adbf298fdc0516b
Sergey Shalaevsky <shalaevskiy@nemosoft.ru>
2020-03-12 15:44:19 +0300

    changed: materialized gateway

--- src/main/webapp/app/admin/gateway/gateway.component.ts
+++ src/main/webapp/app/admin/gateway/gateway.component.ts
@@ -1,7 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 
 import { GatewayRoutesService } from './gateway-routes.service';
 import { GatewayRoute } from './gateway-route.model';
+import { MatTableDataSource } from '@angular/material/table';
+import { MatSort, MatSortable } from '@angular/material/sort';
 
 @Component({
   selector: 'jhi-gateway',
@@ -11,17 +13,24 @@ import { GatewayRoute } from './gateway-route.model';
 export class GatewayComponent implements OnInit {
   gatewayRoutes: GatewayRoute[] = [];
   updatingRoutes = false;
+  displayedColumns: string[] = ['path', 'serviceId', 'server'];
+  dataSource = new MatTableDataSource(this.gatewayRoutes);
+
+  @ViewChild(MatSort, { static: true }) sort: MatSort = new MatSort();
 
   constructor(private gatewayRoutesService: GatewayRoutesService) {}
 
   ngOnInit(): void {
     this.refresh();
+    this.sort.sort({ id: 'serviceId', start: 'asc' } as MatSortable);
+    this.dataSource.sort = this.sort;
   }
 
   refresh(): void {
     this.updatingRoutes = true;
     this.gatewayRoutesService.findAll().subscribe(gatewayRoutes => {
       this.gatewayRoutes = gatewayRoutes;
+      this.dataSource = new MatTableDataSource(this.gatewayRoutes);
       this.updatingRoutes = false;
     });
   }
--- src/main/webapp/app/admin/gateway/gateway.module.ts
+++ src/main/webapp/app/admin/gateway/gateway.module.ts
@@ -5,9 +5,13 @@ import { EltraGatewaySharedModule } from 'app/shared/shared.module';
 import { GatewayComponent } from './gateway.component';
 
 import { gatewayRoute } from './gateway.route';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatTableModule } from '@angular/material/table';
+import { MatSortModule } from '@angular/material/sort';
 
 @NgModule({
-  imports: [EltraGatewaySharedModule, RouterModule.forChild([gatewayRoute])],
+  imports: [EltraGatewaySharedModule, RouterModule.forChild([gatewayRoute]), MatCardModule, MatButtonModule, MatTableModule, MatSortModule],
   declarations: [GatewayComponent]
 })
 export class GatewayModule {}
